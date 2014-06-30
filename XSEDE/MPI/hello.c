213ad7834aa1399a41d2ed5c7a650573a4617023
Guangfeng <zhou1269@gmail.com>
2014-06-30 11:27:36 -0400

    fixed a bug in hello.c

--- XSEDE/MPI/hello.c
+++ XSEDE/MPI/hello.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include "mpi.h"
+#include <mpi.h>
 
-main(int argc, char** argv){
+int main(int argc, char** argv){
     int my_PE_num;
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&my_PE_num);
-    printf("Hello World, PE num %d.\n",&my_PE_num;
+    printf("Hello World, PE num %d.\n",my_PE_num);
     MPI_Finalize();
+    return 0;
 }
