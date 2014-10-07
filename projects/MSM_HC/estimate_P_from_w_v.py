b0fa1620cad0a671d0378dc70845180ee82cdd95
gfzhou <gfzhou01@gmail.com>
2014-10-07 14:37:49 -0400

    Major change in  estimate_P_from_w_v.py

--- projects/MSM_HC/estimate_P_from_w_v.py
+++ projects/MSM_HC/estimate_P_from_w_v.py
@@ -7,13 +7,28 @@ import LifsonRoigTools as LRTools
 import numpy as np
 from collections import Counter
 
-v = 0.351
-w = 1.615
+l = np.loadtxt('/Users/tud51931/voelzlab/analysis/LifsonRoig/scripts/test_Fs_RRR_ff03Loglikelihood.dat')
+w = np.loadtxt('/Users/tud51931/voelzlab/analysis/LifsonRoig/scripts/test_Fs_RRR_ff03w_params.dat')
+v = np.loadtxt('/Users/tud51931/voelzlab/analysis/LifsonRoig/scripts/test_Fs_RRR_ff03v_params.dat')
+
+I = np.argsort(l)
+w_max = w[I[-1]]
+v_max = v[I[-1]]
 
 assignment = io.loadh('/Volumes/Guangfeng/Fs-peptide/Fs-ff03-owlsnest/ff03-RRR-full-msm/Assignments.h5.RRR','arr_0')
 project = Project.load_from('/Volumes/Guangfeng/Fs-peptide/Fs-ff03-owlsnest/ff03-RRR-full-msm/ProjectInfo.yaml')
 c = Counter(assignment.reshape(1,-1)[0])
 populations = np.zeros(np.max(c.keys())+1)
+
+def calculate_weight_frame(w_array,v_array,w_param,v_param):
+    weight = 1.0
+    for i in w_array*w_param:
+        if i != 0:
+            weight = weight*i
+    for i in v_array*v_param:
+        if i != 0:
+            weight = weight*i
+    return weight
 
 for i in range(project.n_trajs):
     print "Working on: %s"%project.traj_filename(i)
@@ -21,15 +36,17 @@ for i in range(project.n_trajs):
     hc = LRTools.LifsonRoigTools()
     hc.prepare(traj)
     hc.ConvertDihedralsToHCStrings(sequencetype='peptide',helixtype='alpha')
-    hc.count_Helix_per_frame()
+    hc.calculate_w_v_array()
+
     for j in range(assignment[i].shape[0]):
         if assignment[i][j] != -1:
-            populations[assignment[i][j]] = populations[assignment[i][j]] + (w**hc.helix_w_numbers_frame[j])*(v**hc.helix_v_numbers_frame[j])
+            weight = calculate_weight_frame(hc.w_array[j],hc.v_array[j],w_max,v_max)
+            populations[assignment[i][j]] = populations[assignment[i][j]] + weight
 
 # weigh populations by the counts
 for i in range(populations.shape[0]):
     populations[i] = populations[i]/c[i]
 populations = populations/populations.sum()
 print populations
-np.savetxt('Populations.LifsonRoig.dat',populations)
+np.savetxt('Populations.LifsonRoig.new.dat',populations)
 
