cf2ef90e1c554c3efa37203c1dd92308a40379b0
gfzhou <gfzhou01@gmail.com>
2016-10-10 11:30:53 -0400

    Added usechimera_dock_result.py

---
+++ tutorials/chimera/usechimera_dock_result.py
@@ -0,0 +1,21 @@
+#------------------------------------------
+#Written by Guangfeng Zhou
+#Dr.Voelz Lab
+#Chemistry Department
+#Temple University
+#October,25,2012
+#-------------------------------------------
+import os,sys
+import chimera
+from chimera import runCommand as rc
+
+for i in range(1):
+    for j in range(1):
+        recptor = '../../MurA-MSM-mol2/state%d-%d.aligned.mol2'%(i,j)
+        ligand = 'MurA-albo-dock/state%d-%d/rigid-xtal1_scored.mol2'%(i,j)
+        outfile = 'state%d-%d-albo1.pdb'%(i,j)
+        rc("open %s"%recptor)
+        rc("open %s"%ligand)
+        rc("combine #0,1.1")
+        rc("write format pdb 2 %s"%outfile)
+        rc("close all")
